290946c1a5e6255dbda5bdea8c1c6d176069afa2
1129830650@qq.com <1129830650@qq.com>
2018-05-22 17:28:16 +0800

    整合测试了Thymeleaf

--- src/main/java/com/tu/manager/controller/Index1.java
+++ src/main/java/com/tu/manager/controller/Index1.java
@@ -3,15 +3,15 @@ package com.tu.manager.controller;
 import com.tu.manager.annotation.DataSource;
 import com.tu.manager.dao.AdminDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 //@EnableJpaRepositories("com.tu.manager.dao")
 //@EntityScan("com.tu.manager.entity")
-@RestController
+@Controller
 public class Index1 {
 
     @RequestMapping(value="/hello",method = RequestMethod.GET)
@@ -25,16 +25,18 @@ public class Index1 {
 
     @RequestMapping(value="/jpa1",method = RequestMethod.GET)
     @DataSource(name = "dataSource1")
-    public String jpa1(){
+    public String jpa1(Map<String,Object> map){
         Map<String, String> stringStringMap = adminDao.find();
-        return stringStringMap.get("realname");
+        map.put("test",stringStringMap.get("realname"));
+        return "index";
     }
 
     @RequestMapping(value="/jpa2",method = RequestMethod.GET)
     @DataSource(name = "dataSource2")
-    public String jpa2(){
+    public String jpa2(Map<String,Object> map){
         Map<String, String> stringStringMap = adminDao.find();
-        return stringStringMap.get("realname");
+        map.put("test",stringStringMap.get("realname"));
+        return "index";
     }
 
 }
