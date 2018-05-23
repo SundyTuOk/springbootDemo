e30cf3079602403de011473b0f3c8c42a2aa9868
SundyTuok <1129830650@qq.com>
2018-05-23 22:35:38 +0800

    整合了vue

--- src/main/java/com/tu/manager/controller/Index1.java
+++ src/main/java/com/tu/manager/controller/Index1.java
@@ -2,10 +2,12 @@ package com.tu.manager.controller;
 
 import com.tu.manager.annotation.DataSource;
 import com.tu.manager.dao.AdminDao;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -25,10 +27,14 @@ public class Index1 {
 
     @RequestMapping(value="/jpa1",method = RequestMethod.GET)
     @DataSource(name = "dataSource1")
-    public String jpa1(Map<String,Object> map){
+    @ResponseBody
+    public String jpa1(Map<String,Object> map,int length){
+        System.out.println(length);
         Map<String, String> stringStringMap = adminDao.find();
         map.put("test",stringStringMap.get("realname"));
-        return "index";
+        JSONObject json = new JSONObject();
+        json.put("test",stringStringMap.get("realname"));
+        return json.toString();
     }
 
     @RequestMapping(value="/jpa2",method = RequestMethod.GET)
