91f801ebdb2bc27989e0761f0ba797c60345b623
1129830650@qq.com <1129830650@qq.com>
2018-05-25 17:47:53 +0800

    添加redis做数据库缓存

---
+++ src/main/java/com/tu/manager/config/dbconfig/RedisCache.java
@@ -0,0 +1,44 @@
+package com.tu.manager.config.dbconfig;
+
+import org.apache.ibatis.cache.Cache;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.locks.ReadWriteLock;
+
+@Component
+public class RedisCache implements Cache {
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public void putObject(Object o, Object o1) {
+
+    }
+
+    @Override
+    public Object getObject(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object removeObject(Object o) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public ReadWriteLock getReadWriteLock() {
+        return null;
+    }
+}
